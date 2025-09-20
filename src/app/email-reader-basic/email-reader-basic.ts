6efdf26b2cd3fca267aced6f4477d41a0f8981b7
Lakmi <lakmi94@gmail.com>
2025-09-20 02:16:23 +0200

    add functions

--- src/app/email-reader-basic/email-reader-basic.ts
+++ src/app/email-reader-basic/email-reader-basic.ts
@@ -1,15 +1,18 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Email } from '../interfaces/email';
+import { CommonModule } from '@angular/common';  // 👈 add this
+import { log } from 'console';
 
 @Component({
   selector: 'app-email-reader-basic',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './email-reader-basic.html',
   styleUrl: './email-reader-basic.css',
 })
 export class EmailReaderBasic implements OnInit {
-  email: Email 
+  email: Email;
+  emailList: Email[] = [];
   @ViewChild('emailForm') emailForm: any;
   constructor() {}
 
@@ -20,5 +23,24 @@ export class EmailReaderBasic implements OnInit {
       subject: '',
       body: null,
     };
+    this.emailList = [];
+  }
+
+  addEmailToList(email: Email): void {
+      console.log('email',email);
+    this.emailList.push({
+      to: email.to,
+      from: email.from,
+      subject: email.subject,
+      body: email.body,
+    });
+    this.emailForm.resetForm();
+    console.log(this.emailList);
+    console.log(this.email);
+    
+  }
+
+  resetEmail(): void {
+    this.emailForm.resetForm();
   }
 }
