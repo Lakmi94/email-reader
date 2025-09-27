0c3e689d288be936b1269d12cc4188d7b0b74993
floorvansteijn <vansteijn.floor@gmail.com>
2025-09-27 16:54:35 +0200

    Checkbox body filter

--- src/app/emails-list/emails-list.ts
+++ src/app/emails-list/emails-list.ts
@@ -3,21 +3,32 @@ import { Email } from '../interfaces/email';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { EmailService } from '../services/email-service';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-emails-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './emails-list.html',
   styleUrls: ['./emails-list.css'],
 })
 export class EmailsList implements OnInit {
   emailList: Array<Email> = [];
+  withBody: boolean = false;
   email: Email | null = null;
 
   constructor(private emailService: EmailService) {}
   ngOnInit(): void {
     this.emailService.getEmails().subscribe((list) => (this.emailList = list));
   }
+
+  get filteredEmails(): Email[] {
+    if (this.withBody) {
+      return this.emailList.filter(email => !!email.body && email.body.trim() !== '');
+    }
+    return this.emailList;
+  }
+  
 
   getEmailsList() {
     this.emailService.getEmails().subscribe((list) => {
